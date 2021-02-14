725023dc97a83a4567438fe671793ded46f0196d
Vyctor Huggo <vyctorhuggo322@gmail.com>
2021-02-14 00:28:29 -0300

    Código atualizado e funcional

---
+++ Codigo/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Projeto1
+{
+    class Program
+    {
+        public static Char[] delimitadores = { '.', 'x', 'e', '^' };
+        static void Main()
+        {
+            Console.WriteLine("[1] Para calcular a [Quantidade de Elétrons] (Q = n.e)");
+            Console.WriteLine("[2] Para calcular o [Número de Elétrons] (n = Q/e)");
+            Console.Write("O que você deseja: "); 
+            int X = Convert.ToInt32(Console.ReadLine());
+            switch (X)
+            {
+                case 1:
+                    try
+                    {
+                        Console.Write("Qual é o valor de N: ");
+                        var CompN = Console.ReadLine().Split(delimitadores);
+                        Double N = double.Parse(CompN[0]);
+                        int baseN = int.Parse(CompN[1]);
+                        int ExpoN = int.Parse(CompN[2]);        
+                        double x = N*1.6;
+                        int y = ExpoN + -19;
+                        soluction(x, y, baseN, N, ExpoN, "case1");
+                    } 
+                    catch
+                    {
+                        Console.Write("Error:Valor não aceito analise o número e tente novamente");
+                        Main();
+                    }
+                    break;
+
+                case 2:
+                    try
+                    {
+                        Console.Write("Qual é o valor de Q: ");
+                        var CompQ = Console.ReadLine().Split(delimitadores);
+                        Double Q = double.Parse(CompQ[0]);
+                        int baseQ = int.Parse(CompQ[1]);
+                        int ExpoQ = int.Parse(CompQ[2]);
+                        
+                        double x1 = Q/1.6;
+                        int y1 = ExpoQ -(-19);
+                        soluction(x1, y1, baseQ, Q, ExpoQ, "case2");
+                    } 
+                    catch
+                    {
+                        Console.Write("Error:Valor não aceito analise o número e tente novamente.");
+                        Main();
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("|----------------|\n" + "|Número inválido!|\n" + "|----------------|");
+                    Main();
+                    break;
+            }
+        }
+        static void soluction(double a, int b, int c, double d, int e, string name)
+        {
+            if(name == "case1")
+            {
+                Console.WriteLine("O corpo ganha(-) ou perde(+) elétrons?");
+                string G = Console.ReadLine();
+                G = G.ToUpper();
+
+                switch(G)
+                {
+                    case "GANHA":
+                        Console.WriteLine("\n");
+                        Console.WriteLine($"Q = {d}.{c}^{e}.1,6.10^-19");
+                        Console.WriteLine($"Q = {d}.1,6.{c}^{e}.10^-19");
+                        Console.WriteLine($"Q =  {-a:f2}.{c}^{b} C");
+                        break;
+
+                    case "G":
+                        Console.WriteLine("\n");
+                        Console.WriteLine($"Q = {d}.{c}^{e}.1,6.10^-19");
+                        Console.WriteLine($"Q = {d}.1,6.{c}^{e}.10^-19");
+                        Console.WriteLine($"Q =  {-a:f2}.{c}^{b} C");
+                        break;
+
+                    case "PERDE":
+                        Console.WriteLine("\n");
+                        Console.WriteLine($"Q = {d}.{c}^{e}.1,6.10^-19");
+                        Console.WriteLine($"Q = {d}.1,6.{c}^{e}.10^-19");
+                        Console.WriteLine($"Q =  {a:f2}.{c}^{b} C");
+                        break;
+
+                    case "P":
+                        Console.WriteLine("\n");
+                        Console.WriteLine($"Q = {d}.{c}^{e}.1,6.10^-19");
+                        Console.WriteLine($"Q = {d}.1,6.{c}^{e}.10^-19");
+                        Console.WriteLine($"Q =  {a:f2}.{c}^{b} C");
+                        break;
+                }
+            }
+            if(name == "case2")
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine($"N = {d}.{c}^{e}/1,6.10^-19");
+                Console.WriteLine($"N = {d}/1,6.{c}^{e}/10^-19");
+
+                if(a < 0)
+                {
+                    Console.WriteLine($"N = {-1*a:f2}.{c}^{b} C");
+                    Console.WriteLine("Ganha");
+                }
+                else
+                {
+                    Console.Write($"N = {a:f2}.{c}^{b} C" + " (Perde)");
+                }
+            }
+        }
+    }
+}
